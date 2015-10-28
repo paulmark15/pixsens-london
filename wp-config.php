70a2ddabcf6628e13400d82fe267172acdb71270
PaulMark <pavel.markevic1@gmail.com>
2015-10-28 14:47:46 +0200

    change of db

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'pixsens');
+define('DB_NAME', 'pixsens_db');
 
 /** MySQL database username */
-define('DB_USER', 'paulm');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'qwewer123');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
